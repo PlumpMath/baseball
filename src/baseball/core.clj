218811118e664b36eb8fd8691f9b32a2e449b000
Neo Cincinnati <pairing@edgecase.com>
2014-04-08 19:47:28 -0400

    Throw it back and forth a bunch

--- src/baseball/core.clj
+++ src/baseball/core.clj
@@ -1,19 +1,26 @@
 (ns baseball.core
   (:require [clojure.core.async :refer (chan >!! <!! >! <! close! go go-loop timeout alt! put!)]))
+(declare you)
 
 (defn make-player
-  [action]
+  [max-cnt action]
   (let [ch (chan)]
-    (go-loop []
-      (let [msg (<! ch)]
-        (action msg)))
+    (go-loop
+      [cnt max-cnt]
+      (when (pos? cnt)
+        (let [msg (<! ch)]
+          (action msg)
+          (recur (dec cnt)))))
     ch))
 
 ;player one loop
-(def me (make-player (fn [msg] (println msg))))
+(def me (make-player 10
+                     #(do
+                        (println %)
+                        (put! you %))))
 
 ;player two loop
-(def you (make-player #(put! me %)))
+(def you (make-player 10 #(put! me %)))
 
 (>!! you "test")
 
