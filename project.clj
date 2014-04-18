4ac6728dd113d97848776f98b40af30f3f1e5d4a
Geoff Lane <geoff@zorched.net>
2014-04-18 15:34:26 -0400

    Refactor into a player protocol and make main worker handle multiple message types
    Signed-off-by: Geoff Lane <geoff@zorched.net>

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]])
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
+                 [org.clojure/core.match "0.2.1"]])
--- src/baseball/core.clj
+++ src/baseball/core.clj
@@ -1,25 +1,75 @@
 (ns baseball.core
-  (:require [clojure.core.async :refer (chan >!! <!! >! <! close! go go-loop timeout alt! put!)]))
+  (:require [clojure.core.async :refer (chan >!! <!! >! <! close! go go-loop)]
+            [clojure.core.match :refer (match)]))
 
-(defn make-player
-  [max-cnt action]
-  (let [ch (chan)]
+(defprotocol Player
+  (player-name [p])
+  (walk [p] [p paces])
+  (throw-to [p ball])
+  (play [p])
+  (done [p]))
+
+(deftype LazyLobber [ch nm max-cnt partner]
+  Player
+  (player-name [p] nm)
+  (walk [p] (walk p 20))
+  (walk [p paces]
+    (future
+      (println "Walking to position")
+      (Thread/sleep (* 100 paces))
+      (println "Ready to play")
+      (>!! partner [:ready p])))
+  (throw-to [p ball] (>!! ch ball))
+  (done [p]
+    (println (str nm " is done playing."))
+    (close! ch))
+  (play [p]
     (go-loop
       [cnt max-cnt]
-      (when (pos? cnt)
-        (let [msg (<! ch)]
-          (action ch msg)
-          (recur (dec cnt)))))
-    ch))
+      (let [msg (<! ch)]
 
-;player one loop
-(def me (make-player 10000
-                     #(do
-                        (println (:ball %2))
-                        (put! (:channel %2) (:ball %2)))))
+       (if (pos? cnt)
+         (do
+           (println (str nm " got the ball (" cnt "). Throwing back."))
+           (>! partner [:ball p "Lazy Lob"])
+           (recur (dec cnt)))
+         (>! partner [:stop p]))))
+    p))
+
+(def players (atom []))
+(defn start-playing [p]
+  (swap! players conj p)
+  (throw-to p "Ball"))
+
+(defn stop-playing [p me]
+  (swap! players #(remove (fn [p1] (= (player-name p) (player-name p1))) %))
+  (done p))
+
+(defn make-me
+  []
+  (let [me-ch (chan)]
+    (go-loop
+      []
+      (let [ball (<! me-ch)]
+        ;; (println ball)
+        (match [ball]
+               [[:ready p]] (start-playing p)
+               [[:stop p]] (stop-playing p me-ch)
+               [[:ball p msg]] (throw-to p msg)
+               :else (println "Got some message I don't know how to handle"))
+
+        (if (empty? @players)
+          (println "Game over, time to go home.")
+          (recur))))
+    me-ch))
 
 ;player two loop
-(doseq [x (range 10)]
-  (let [ch (make-player 10 #(put! me {:channel %1, :ball %2}))]
-    (>!! ch (str "ball" x))))
+(defn run []
+  (let [me (make-me)
+        friend1 (LazyLobber. (chan) "Friend1" 30 me)
+        friend2 (LazyLobber. (chan) "Friend2" 15 me)
+        friend3 (LazyLobber. (chan) "Friend3" 5 me)]
+
+    (doseq [f [friend1 friend2 friend3]]
+      (walk (play f)))))
 
